f1de35f3ebb443456d514f0cf40f313bac12a70a
Janette Arrakis <jarrakis@il-g3.msk.21-school.ru>
2021-11-12 15:28:35 +0300

    byak

--- get_next_line.c
+++ get_next_line.c
@@ -6,6 +6,21 @@
 /*   By: jarrakis <jarrakis@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 18:52:17 by jarrakis          #+#    #+#             */
-/*   Updated: 2021/11/11 18:52:17 by jarrakis         ###   ########.fr       */
+/*   Updated: 2021/11/12 14:53:36 by jarrakis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include<stdio.h> // use print for test begin del
+#include<unistd.h>
+#include<fcntl.h> // use open for test begin del
+#include<stdlib.h>
+#include"get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+}
+
+/*
+ * main for test
+ */
+
